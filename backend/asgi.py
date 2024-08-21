48924f75ceb0b7e397d678978f7f8be058029596
salar403 <salar40340@gmail.com>
2024-08-22 02:51:40 +0330

    bug fix at asgi settings

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,17 +1,7 @@
 import os, django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-from backend.middlewares.websocket import AuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
 
-import exchange.routing
-
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": AuthMiddleware(URLRouter(exchange.routing.websocket_urlpatterns)),
-    }
-)
+application = get_asgi_application()
