0ea3c9ae60155c717e9ac4285ea34d368d730098
salar403 <salar40340@gmail.com>
2024-08-21 13:52:42 +0330

    added order models

--- exchange/models.py
+++ exchange/models.py
@@ -1,10 +1,15 @@
+from http.client import PROCESSING
 from django.db import models
+
+from user.models import User
 
 
 class Exchange(models.Model):
-    BINANCE = 1
+    ABAN = 1
+    BINANCE = 2
 
     EXCHANGE_TYPES = [
+        (ABAN, "aban"),
         (BINANCE, "binance"),
     ]
 
@@ -92,3 +97,99 @@ class AssetPair(models.Model):
 
     class Meta:
         unique_together = ("base_asset", "quote_asset")
+
+    @property
+    def symbol(self):
+        return f"{self.base_asset.symbol}_{self.quote_asset.symbol}"
+
+
+class Order(models.Model):
+    BUY = 1
+    SELL = 2
+    SIDES = [
+        ("buy", BUY),
+        ("sell", SELL),
+    ]
+
+    CREATED = 1
+    PROCESSING = 2
+    FILLED = 3
+    SUCCESS = 4
+    FAILED = 5
+    UNKNOWN = 6
+
+    STATE_CHOICES = [
+        ("created", CREATED),
+        ("processing", PROCESSING),
+        ("filled", FILLED),
+        ("success", SUCCESS),
+        ("failed", FAILED),
+        ("unknown", UNKNOWN),
+    ]
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        null=False,
+        related_name="orders",
+    )
+    asset_pair = models.ForeignKey(
+        to=AssetPair,
+        on_delete=models.CASCADE,
+        null=False,
+        related_name="orders",
+    )
+    amount = models.DecimalField(max_digits=40, decimal_places=20, null=False)
+    side = models.IntegerField(choices=SIDES, null=False)
+    status = models.IntegerField(choices=STATE_CHOICES, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class ExchangeOrder(models.Model):
+    CREATED = 1
+    PROCESSING = 2
+    SUCCESS = 3
+    FAILED = 4
+    UNKNOWN = 5
+    STATE_CHOICES = [
+        ("created", CREATED),
+        ("processing", PROCESSING),
+        ("success", SUCCESS),
+        ("failed", FAILED),
+        ("unknown", UNKNOWN),
+    ]
+    exchange = models.ForeignKey(
+        to=Exchange,
+        on_delete=models.CASCADE,
+        null=False,
+        related_name="orders",
+    )
+    order = models.ForeignKey(
+        to=Order,
+        on_delete=models.CASCADE,
+        null=False,
+        related_name="exchange_order",
+    )
+    status = models.IntegerField(choices=STATE_CHOICES, null=False)
+    result = models.JSONField(null=True)
+
+
+class MockOrder(Order):
+    WAITING_FOR_MORE = 1
+    AGGREGATING = 2
+    DONE = 3
+
+    MOCK_STATE = [
+        (WAITING_FOR_MORE, "waiting"),
+        (AGGREGATING, "aggregating"),
+        (DONE, "done"),
+    ]
+
+    value = models.DecimalField(max_digits=40, decimal_places=20, null=False)
+    mock_status = models.IntegerField(choices=MOCK_STATE, default=WAITING_FOR_MORE)
+    aggregated_order = models.ForeignKey(
+        to=Order,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="sub_orders",
+    )
